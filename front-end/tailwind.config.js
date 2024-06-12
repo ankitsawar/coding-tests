ed59d60697c705736476166e3571933097e2e325
Ankit <ankit@Ankits-MacBook-Air.local>
2024-06-13 01:27:08 +0530

    Designed a web page not fully completed

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans']
+      },
+      backgroundImage: {
+        'pricing_card_bullet': "url('../images/bullet.svg')",
+        'footer-effects': "url('../images/effect-lines.png')"
+      }
+    },
+    colors: {
+      'text-brand': '#54565A'
+    }
   },
   plugins: [],
 }
